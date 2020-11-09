1d3a8a51c93cba337a206e17bcf57875eec7e9d4
James Flesher <james.flesher@insuresign.com>
2020-11-08 21:14:41 -0500

    adding prettier config

--- App.tsx
+++ App.tsx
@@ -1,30 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Amplify from "aws-amplify";
+import Amplify from 'aws-amplify';
 // @ts-ignore
-import config from "./aws-exports";
+import config from './aws-exports';
 // @ts-ignore
-import { withAuthenticator } from "aws-amplify-react-native";
+import { withAuthenticator } from 'aws-amplify-react-native';
 
 Amplify.configure(config);
 
 function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
 
-export default withAuthenticator(App, true)
-
+export default withAuthenticator(App, true);
